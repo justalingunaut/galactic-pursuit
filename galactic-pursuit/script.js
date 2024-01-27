6830a15af1378f388cdb8986b8b2ff080edabcd5
Umut Gülaç <umutgulac31@gmail.com>
2024-01-27 21:36:59 +0300

    Update script.js

--- galactic-pursuit/script.js
+++ galactic-pursuit/script.js
@@ -19,6 +19,24 @@ window.addEventListener('load',function(){
     }
 
     class Player {
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+
+        }
+        update(){
+            this.y += this.speedY;
+
+        }
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+
+
+        }
 
     }
     
@@ -39,6 +57,21 @@ window.addEventListener('load',function(){
     }
 
     class Game {
-        
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update(){
+            this.player.update();
+
+        }
+
+        draw(context){
+            this.player.draw(context);
+
+        }
     }
+
+    const game = new Game();
 });
